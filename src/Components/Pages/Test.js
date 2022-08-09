e4e90c2e2a048dd9aee2910ded49dd338f3e3db5
Devin Jansa <devin_jansa@icloud.com>
2022-08-08 20:19:44 -0700

    added some color

--- src/Components/Pages/Test.js
+++ src/Components/Pages/Test.js
@@ -8,26 +8,25 @@ const Test = function(props) {
     )
     const silvieImage = silvie.map(image =>
         <div className="format">
-        <div className="icon" key={image.id}>
-            <img src={image.image} /><br/>
-            <font className="phone">(206) 779-0530</font><br/>
-            <a className="email" href="mailto:Silvie.Opatrna@gmail.com">Silvie.Opatrna@gmail.com</a><br/>
-        </div>
-        <div className="text">
-            <div className="quote">
-                '{image.quote}'
+            <div className="icon" key={image.id}>
+                <img className='silvie_image' src={image.image} /><br/>
+                <font className="phone">(206) 779-0530</font><br/>
+                <a className="email" href="mailto:Silvie.Opatrna@gmail.com">Silvie.Opatrna@gmail.com</a><br/>
             </div>
-            <div className="student">
-                - {image.student},&nbsp;
-                {image.occupation}
+            <div className="text">
+                <div className="quote">
+                    "{image.quote}""
+                </div>
+                <div className="student">
+                    - {image.student},&nbsp;
+                    {image.occupation}
+                </div>
             </div>
-        </div>
         </div>
     )
     
     return (
         <div className="format">
-
             {silvieImage[0]}
         </div>
     )
