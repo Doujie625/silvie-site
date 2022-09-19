723f8cfed48895fdb5909a632533209fe55c4f54
Devin Jansa <devin_jansa@icloud.com>
2022-09-19 13:33:39 -0700

    made more changes to books, looks good, but is not yet responsive

--- src/Components/Data/Book_data.js
+++ src/Components/Data/Book_data.js
@@ -3,6 +3,17 @@ import ce2 from "./../../images_books/CE2.jpg"
 import ce3 from "./../../images_books/CE3.jpg"
 import CV401 from "./../../images_books/401CV.jpg"
 import FDC from "./../../images_books/FDC.jpg"
+import CKZK1 from "./../../images_books/CKZK1.webp"
+import CKZK2 from "./../../images_books/CKZK2.jpg"
+import Povidani from "./../../images_books/Povidani.jpg"
+import Prazke from "./../../images_books/Prazke.jpeg"
+import Povidky from "./../../images_books/Povidky.jpg"
+import Brneske from "./../../images_books/Brneske.jpeg"
+import FF from "./../../images_books/Fluent_forever.webp"
+import BF from "./../../images_books/Becoming_fluent.jpg"
+import FI3 from "./../../images_books/Fluent_in_3.webp"
+import MS from "./../../images_books/Make_stick.jpg"
+import Einstein from "./../../images_books/Einstein.jpg"
 
 export const book_data = [
     {
@@ -19,6 +30,7 @@ export const book_data = [
         url: 'https://www.amazon.com/Cestina-Expres-Czech-Express-Pack/dp/8087481267/ref=sr_1_1?keywords=%C4%8De%C5%A1tina+express+2&qid=1572160772&sr=8-1',
         author: "Lída Holá, Pavla Bořilová"
     },
+
     {
         id: 3,
         title: 'Čeština Expres 3',
@@ -35,97 +47,94 @@ export const book_data = [
     },
     {
         id: 5,
-        title: 'A Frequency Dictionary of Czech: Core Vocabulary for Learners',
+        title: 'A Frequency Dictionary of Czech:',
+        subtitle: "Core Vocabulary for Learners",
         image: FDC,
         url: 'https://www.amazon.com/Frequency-Dictionary-Czech-Vocabulary-Dictionaries/dp/0415576628/ref=sr_1_1?keywords=A+Frequency+Dictionary+of+Czech%3A+Core+Vocabulary+for+Learners+by+František+Čermák+and+Michal+Křen&qid=1663264385&s=books&sr=1-1',
         author: "František Čermák and Michal Křen"
     },
+
     {
         id: 6,
         title: 'Česky krok za krokem 1',
-        image: '',
-        url: '',
+        image: CKZK1,
+        url: 'https://www.amazon.com/Czech-Step-Textbook-Appendix-Multilingual/dp/8074701298/ref=sr_1_1?qid=1663264451&refinements=p_27%3AHola+Lida&s=books&sr=1-1&text=Hola+Lida',
         author: "Lída Holá"
     },
     {
         id: 7,
         title: 'Česky krok za krokem 2',
-        image: '',
-        url: '',
+        image: CKZK2,
+        url: 'https://www.amazon.com/New-Czech-Step-v-English/dp/8086903923/ref=pd_rhf_d_se_s_pd_crcd_sccl_1_16/140-0175372-5249052?pd_rd_w=s8WbD&content-id=amzn1.sym.cee83ff1-8fc1-4533-a3f5-bf3d998f4558&pf_rd_p=cee83ff1-8fc1-4533-a3f5-bf3d998f4558&pf_rd_r=2H5K5VVC47AE9JWCRF9X&pd_rd_wg=vy09x&pd_rd_r=5610c6fc-1fa1-45f8-8ce3-208d596277f3&pd_rd_i=8086903923&psc=1',
         author: "Lída Holá"
     },
+
     {
         id: 8,
         title: 'O pejskovi a kočičce',
-        image: '',
-        url: '',
+        subtitle: "adapted by Silvie Převrátilová and Petra Bulejčíková",
+        image: Povidani,
+        url: 'https://www.czechstepbystep.cz/en/detail-ucebnice/o-pejskovi-a-kocicce',
         author: "Josef Čapek"
     },
     {
         id: 9,
-        title: 'O pejskovi a kočičce',
-        subtitle: "adapted by Silvie Převrátilová and Petra Bulejčíková",
-        image: '',
-        url: '',
-        author: "Josef Čapek"
-    },
-    {
-        id: 10,
         title: 'Pražské legendy',
         subtitle: "adapted by Lída Holá",
-        image: '',
-        url: '',
-        author: ""
+        image: Prazke,
+        url: 'https://www.czechstepbystep.cz/en/detail-ucebnice/prazske-legendy',
+        author: "František Langer"
     },
     {
         id: 11,
         title: 'Povídky malostranské',
         subtitle: "adapted by Lída Holá",
-        image: '',
-        url: '',
+        image: Povidky,
+        url: 'https://www.czechstepbystep.cz/en/detail-ucebnice/povidky-malostranske',
         author: "Jan Neruda"
     },
     {
         id: 12,
         title: 'Brněnské legendy',
         subtitle: "adapted by Martina Trchová",
-        image: '',
-        url: '',
+        image: Brneske,
+        url: 'https://www.czechstepbystep.cz/detail-ucebnice/brnenske-legendy',
         author: ""
     },
+
     {
         id: 13,
         title: 'Fluent Forever: How to Learn Any Language Fast and Never Forget It',
-        image: '',
-        url: '',
+        image: FF,
+        url: 'https://www.amazon.com/Fluent-Forever-Learn-Language-Forget/dp/0385348118/ref=tmm_pap_swatch_0?_encoding=UTF8&qid=1663618537&sr=8-1',
         author: "Gabriel Wyner"
     },
     {
         id: 14,
         title: 'Becoming Fluent: How Cognitive Science Can Help Adults Learn a Foreign Language',
-        image: '',
-        url: '',
+        image: BF,
+        url: 'https://www.amazon.com/Becoming-Fluent-Cognitive-Science-Language/dp/0262529807/ref=tmm_pap_swatch_0?_encoding=UTF8&qid=1663618572&sr=8-1',
         author: "Richard Roberts and Roger Kreuz"
     },
     {
         id: 15,
         title: 'Fluent in 3 Months: How Anyone at Any Age Can Learn to Speak Any Language from Anywhere in the World',
-        image: '',
-        url: '',
+        image: FI3,
+        url: 'https://www.amazon.com/Fluent-Months-Anyone-Language-Anywhere/dp/0062282697/ref=tmm_pap_swatch_0?_encoding=UTF8&qid=1663618590&sr=8-2',
         author: "Benny Lewis"
     },
     {
         id: 16,
         title: 'Make It Stick: The Science of Successful Learning',
-        image: '',
-        url: '',
+        image: MS,
+        url: 'https://www.amazon.com/Make-Stick-Science-Successful-Learning/dp/0674729013/ref=tmm_hrd_swatch_0?_encoding=UTF8&qid=1663618612&sr=8-1',
         author: "Brown, Peter C."
     },
     {
         id: 17,
         title: 'Moonwalking with Einstein: The Art and Science of Remembering Everything',
-        image: '',
-        url: '',
+        image: Einstein,
+        url: 'https://www.amazon.com/Moonwalking-Einstein-Science-Remembering-Everything/dp/0143120530/ref=tmm_pap_swatch_0?_encoding=UTF8&qid=1663618700&sr=8-1',
         author: "Joshua Foer"
     }
 ]
--- src/Components/Pages/Books.js
+++ src/Components/Pages/Books.js
@@ -15,126 +15,139 @@ const Books = function() {
                     <span className="title_words">Books</span>
                 </div><br/>
                 <div className="left"> 
-                I use the following series of textbooks with most of my students:
-                <br/><br/>
-                <div className="book_item">
+                    <div className="stripe">
+                        I use the following series of textbooks with most of my students:
+                    </div>
+                    <div className="book_item">
+                        {book_data.map((book) => {
+                            if (book.id <= 3) 
+                            return (
+                                <div key={book.id} className="book">
+                                    <a href={book.url}>
+                                        <img className="book_image" src={book.image} />
+                                        <div>{book.title}</div>
+                                        <div>{book.author}</div>
+                                        <div>{book.subtitle}</div>
+                                    </a>
+                                </div>
+                            )
+                        })}
+                    </div>
+                    <div className="stripe">
+                        I also recommend
+                    </div>
+                    <div className="book_item">
                     {book_data.map((book) => {
-                        if (book.id <= 3) 
-                        return (
-                            <div className="book" href={book.url}>
-                                <img className="book_image" src={book.image} />
-                                <div>{book.title}</div>
-                            </div>
-                        )
-                    })}
-                </div>
-                <br/><br/>
-                <br/><br/>
+                            if (book.id > 3 && book.id < 6)
+                            return (
+                                <div key={book.id} className="book">
+                                    <a href={book.url}>
+                                        <img className="book_image" src={book.image} />
+                                        <div>{book.title}</div>
+                                        <div>{book.subtitle}</div>
+                                    </a>
+                                </div>
+                            )
+                        })}
+                    </div>
+                    <div className="book_item">
+                    {book_data.map((book) => {
+                            if (book.id > 5 && book.id < 8)
+                            return (
+                                <div key={book.id} className="book">
+                                    <a href={book.url}>
+                                        <img className="book_image" src={book.image} />
+                                        <div>{book.title}</div>
+                                        <div>{book.author}</div>
+                                        <div>{book.subtitle}</div>
+                                    </a>
+                                </div>
+                            )
+                        })}
+                    </div>
+                    <div className="stripe">
+                    Adapted/simplified Czech classics and other Czech books
+                    </div>
+                    <div className="book_item">
+                    {book_data.map((book) => {
+                            if (book.id > 7 && book.id < 13)
+                            return (
+                                <div key={book.id} className="book">
+                                    <a href={book.url}>
+                                        <img className="book_image" src={book.image} />
+                                        <div>{book.title}</div>
+                                        <div>{book.author}</div>
+                                        <div>{book.subtitle}</div>
+                                    </a>
+                                </div>
+                            )
+                        })}
+                    </div>
+                    <div className="stripe">
+                    Books on strategies for learning a foreign language
+                    </div>
+                    <div className="book_item">
+                    {book_data.map((book) => {
+                            if (book.id > 12 && book.id < 18)
+                            return (
+                                <div key={book.id} className="book">
+                                    <a href={book.url}>
+                                        <img className="book_image" src={book.image} />
+                                        <div>{book.title}</div>
+                                        <div>{book.author}</div>
+                                        <div>{book.subtitle}</div>
+                                    </a>
+                                </div>
+                            )
+                        })}
+                    </div>
 
-                I also recommend
-                <br/><br/>
-
-                {book_data.map((book) => {
-                    if (book.id > 3 && book.id < 6)
-                    return (
-                        <div href={book.url}>{book.title} by {book.author}
-                            <img className="book_image" src={book.image} />
-                        </div>
-                    )
-                })}
-
-                <br/><br/>
-                <br/><br/>
-                Česky krok za krokem 1-2 / Czech Step by Step 1-2 by Lída Holá
-                <br/><br/>
-                <br/><br/>
-                Adapted/simplified Czech classics and other Czech books
-                <br/><br/>
-                Josef Čapek: O pejskovi a kočičce
-                <br/><br/>
-                adapted by Silvie Převrátilová and Petra Bulejčíková
-                <br/><br/>
-                <br/>
-                Pražské legendy
-                <br/>
-                adapted by Lída Holá
-                <br/><br/>
-                Jan Neruda: Povídky malostranské
-                <br/>
-                adapted by Lída Holá
-                <br/><br/>
-                Brněnské legendy
-                <br/>
-                adapted by Martina Trchová
-                <br/><br/>
-                <br/><br/>
-                Books on strategies for learning a foreign language
-                <br/><br/>
-                Fluent Forever: How to Learn Any Language Fast and Never Forget It
-                <br/>
-                by Gabriel Wyner
-                <br/><br/>
-                Becoming Fluent: How Cognitive Science Can Help Adults Learn a Foreign Language
-                <br/>
-                by Richard Roberts and Roger Kreuz
-                <br/><br/>
-                Fluent in 3 Months: How Anyone at Any Age Can Learn to Speak Any Language from Anywhere in the World
-                <br/>
-                by Benny Lewis 
-                <br/><br/>
-                Make It Stick: The Science of Successful Learning
-                <br/>
-                by Brown, Peter C.
-                <br/><br/>
-                Moonwalking with Einstein: The Art and Science of Remembering Everything
-                <br/>
-                by Joshua Foer
     
-                {/* I highly recommend these books:<br/><br/>
+                    {/* I highly recommend these books:<br/><br/>
 
-                Title: Čeština hrou (Czech for Fun)<br/>
-                Author: Susan Kresin<br/>
-                Publisher: McGraw-Hill Primis Custom Publishing<br/>
-                ISBN: 0072455470<br/>
-                Pages: 566<br/>
-                The book is very comprehensive and comes with 2 tapes.<br/><br/>
+                    Title: Čeština hrou (Czech for Fun)<br/>
+                    Author: Susan Kresin<br/>
+                    Publisher: McGraw-Hill Primis Custom Publishing<br/>
+                    ISBN: 0072455470<br/>
+                    Pages: 566<br/>
+                    The book is very comprehensive and comes with 2 tapes.<br/><br/>
 
-                Title: New Czech Step by Step<br/>
-                Author: Lida Hola<br/>
-                Publisher: Akropolis<br/>
-                ISBN: 8086903079<br/>
-                Pages: 240<br/><br/>
+                    Title: New Czech Step by Step<br/>
+                    Author: Lida Hola<br/>
+                    Publisher: Akropolis<br/>
+                    ISBN: 8086903079<br/>
+                    Pages: 240<br/><br/>
 
-                There is also a workbook that students find helpful.<br/>
-                Title: Čeština hrou Pracovní sešit (Czech for Fun Workbook)<br/>
-                Author: Susan Kresin, Hope Subak-Kaspar<br/>
-                Publisher: McGraw-Hill Primis Custom Publishing<br/>
-                ISBN: 0070350132<br/>
-                Pages: 212<br/><br/>
+                    There is also a workbook that students find helpful.<br/>
+                    Title: Čeština hrou Pracovní sešit (Czech for Fun Workbook)<br/>
+                    Author: Susan Kresin, Hope Subak-Kaspar<br/>
+                    Publisher: McGraw-Hill Primis Custom Publishing<br/>
+                    ISBN: 0070350132<br/>
+                    Pages: 212<br/><br/>
 
-                Here is a list of other textbooks for learning the Czech language:<br/><br/>
- */}
-                {/* insert list1 */}
+                    Here is a list of other textbooks for learning the Czech language:<br/><br/>
+                    */}
+                    {/* insert list1 */}
 
-                {/* Advanced textbooks for learning the Czech language:<br/><br/> */}
+                    {/* Advanced textbooks for learning the Czech language:<br/><br/> */}
 
-                {/* insert list2 */}
+                    {/* insert list2 */}
 
-                {/* Czech-English and English-Czech dictionaries:<br/><br/> */}
-                
-                {/* insert list3 */}
+                    {/* Czech-English and English-Czech dictionaries:<br/><br/> */}
+                    
+                    {/* insert list3 */}
 
-                {/* Czech travel phrasebooks:<br/><br/> */}
+                    {/* Czech travel phrasebooks:<br/><br/> */}
 
-                {/* insert list4 */}
+                    {/* insert list4 */}
 
-                {/* Books on strategies for learning a foreign language:<br/><br/> */}
+                    {/* Books on strategies for learning a foreign language:<br/><br/> */}
 
-                {/* insert list5 */}
+                    {/* insert list5 */}
 
-                {/* Other books:<br/><br/> */}
+                    {/* Other books:<br/><br/> */}
 
-                {/* insert list6 */}
+                    {/* insert list6 */}
 
                 </div>
             </div>
